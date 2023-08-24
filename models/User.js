a1ead03881d215723709f3bd4834619965e2bd33
prithvi-JFT <prithvi.raj@jellyfishtechnologies.com>
2023-08-24 17:20:47 +0530

    base setup

--- models/User.js
+++ models/User.js
@@ -1,19 +1,21 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('./db')
 
-const User = sequelize.define('User',{
-    name:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    password:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-})
+const User = sequelize.define('User', {
+  name:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password:{
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+    tableName: 'user', // Set the table name explicitly
+});
 
 module.exports = User;
--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('stripeDB', 'root', 'password', {
+const sequelize = new Sequelize('stripeDB', 'root', 'Prith@12345', {
 	host: 'localhost',
 	dialect: 'mysql',
 });
